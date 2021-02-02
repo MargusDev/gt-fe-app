4eca7ced965000a4e98ea447268ac08f822d10fa
Margus Salk <margus.salk@gmail.com>
2021-02-02 21:52:59 +0200

    Add linting

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      vesrion: 'detect'
+    }
+  },
+  extends: [
+    'react-app',
+    'plugin:react-hooks/recommended'
+  ],
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    quotes: [1, 'single'],
+    semi: 1,
+    'jsx-quotes': [1, 'prefer-single'],
+  }
+};
